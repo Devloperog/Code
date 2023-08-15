2930e71643bf3aa0966c4307aeb8084a766e0886
Devloperog <135986012+Devloperog@users.noreply.github.com>
2023-08-15 23:26:17 +0530

    verify_email.php

--- verify_email.php
+++ verify_email.php
@@ -1,21 +1,34 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    
-    // Generate a random verification code
-    $verificationCode = generateVerificationCode();
-    
-    // Email subject
-    $subject = "Email Verification Code";
-    
-    // Email message
-    $message = "Your verification code is: " . $verificationCode;
-    
-    // Send email using the mail() function
-    if (mail($email, $subject, $message)) {
-        echo "Verification email sent to " . $email;
-    } else {
-        echo "Failed to send verification email.";
+    if (isset($_POST["email"])) {
+        $email = $_POST["email"];
+        
+        // Generate a random verification code
+        $verificationCode = generateVerificationCode();
+        
+        // Email subject
+        $subject = "Email Verification Code";
+        
+        // Email message
+        $message = "Your verification code is: " . $verificationCode;
+        
+        // Set headers
+        $headers = 'From: your_email@example.com' . "\r\n" .
+            'Reply-To: your_email@example.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+        
+        // Send email using the mail() function
+        if (mail($email, $subject, $message, $headers)) {
+            echo "Verification email sent to " . $email;
+        } else {
+            echo "Failed to send verification email.";
+        }
+    } elseif (isset($_POST["verification_code"])) {
+        $verificationCode = $_POST["verification_code"];
+        
+        // Verify the entered verification code (not shown here)
+        
+        echo "Verification successful!";
     }
 }
 
@@ -24,4 +37,3 @@ function generateVerificationCode() {
     return rand(1000, 9999);
 }
 ?>
-
